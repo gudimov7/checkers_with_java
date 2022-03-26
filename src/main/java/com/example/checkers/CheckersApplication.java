961bd17d40a3e52c32bdcd41946622f5e64f924c
Vyacheslav Gudimov <gudimov7@gmail.com>
2022-03-26 08:46:33 +0300

    created the pieces and populated them on board

--- src/main/java/com/example/checkers/CheckersApplication.java
+++ src/main/java/com/example/checkers/CheckersApplication.java
@@ -1,7 +1,6 @@
 package com.example.checkers;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,6 +14,8 @@ public class CheckersApplication extends Application {
     public static final int WIDTH = 8;
     public static final int HEIGHT = 8;
 
+    private Tile[][] board = new Tile[WIDTH][HEIGHT];
+
     private Group tilesGroup = new Group();
     private Group piecesGroup = new Group();
 
@@ -27,11 +28,33 @@ public class CheckersApplication extends Application {
         for (int y = 0; y < HEIGHT; y ++) {
             for (int x = 0; x < WIDTH; x ++) {
                 Tile tile = new Tile((x +y) %2 == 0, x, y);
+                board[x][y] = tile;
                 tilesGroup.getChildren().add(tile);
+
+                //populate pieces
+                Piece piece = null;
+                //populate black
+                if (y <= 2 && (x + y) %2 != 0) {
+                    piece = makePiece(PieceType.BLACK, x, y);
+                }
+                //populate white
+                if (y >= 5 && (x + y) %2 != 0) {
+                    piece = makePiece(PieceType.WHITE, x, y);
+                }
+                if (piece != null) {
+                    tile.setPiece(piece);
+                    piecesGroup.getChildren().add(piece);
+                }
             }
         }
 
         return root;
+    }
+
+    private Piece makePiece(PieceType type, int x , int y) {
+        Piece piece = new Piece(type, x, y);
+
+        return piece;
     }
 
     @Override
--- src/main/java/com/example/checkers/Piece.java
+++ src/main/java/com/example/checkers/Piece.java
@@ -1,6 +1,42 @@
 package com.example.checkers;
 
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+
+import static com.example.checkers.CheckersApplication.TILE_SIZE;
 
 public class Piece extends StackPane {
+
+    private PieceType type;
+
+    public PieceType getType() {
+        return type;
+    }
+
+    public Piece(PieceType type, int x, int y) {
+        this.type = type;
+
+        relocate(x * TILE_SIZE, y * TILE_SIZE);
+
+        //background ellipse
+        Ellipse bg = new Ellipse(TILE_SIZE * 0.3125,TILE_SIZE * 0.26);
+        bg.setFill(Color.BLACK);
+        bg.setStroke(Color.BLACK);
+        bg.setStrokeWidth(TILE_SIZE * 0.03);
+
+        bg.setTranslateX((TILE_SIZE - TILE_SIZE * 0.3125 * 2) /2);
+        bg.setTranslateY((TILE_SIZE - TILE_SIZE * 0.26 * 2) /2 + TILE_SIZE * 0.07);
+
+        //body ellipse
+        Ellipse ellipse = new Ellipse(TILE_SIZE * 0.3125,TILE_SIZE * 0.26);
+        ellipse.setFill(type == PieceType.BLACK ? Color.valueOf("#5a5a5a") : Color.valueOf("#a1a1a1"));
+        ellipse.setStroke(Color.BLACK);
+        ellipse.setStrokeWidth(TILE_SIZE * 0.03);
+
+        ellipse.setTranslateX((TILE_SIZE - TILE_SIZE * 0.3125 * 2) /2);
+        ellipse.setTranslateY((TILE_SIZE - TILE_SIZE * 0.26 * 2) /2);
+
+        getChildren().addAll(bg, ellipse);
+    }
 }
